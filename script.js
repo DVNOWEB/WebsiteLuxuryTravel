d5cb2fc3cc498d4b1c7a205ba1ef4fd4ec7b4049
DVNO <dvno@live.se>
2022-11-24 14:07:49 +0100

    99%

--- script.js
+++ script.js
@@ -25,3 +25,21 @@ owl.owlCarousel({
     autoplayTimeout:2000,
     autoplayHoverPause:true
 });
+
+// Slider comments
+let myIndex = 0;
+carousel();
+
+function carousel() {
+  let i;
+  let x = document.getElementsByClassName('mySlides');
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = 'block';
+  setTimeout(carousel, 9000);
+}
